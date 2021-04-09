607598b4ffa602298ba6878e41470a86e03e61b8
Leon Bogaert <leon@tim-online.nl>
2021-04-09 15:01:07 +0200

    Fix typo

--- entry.go
+++ entry.go
@@ -3,25 +3,25 @@ package cegid
 // fixed:"{startPos},{endPos},[{alignment},[{padChar}]]"
 type Entry struct {
 	JOURNAL             string `fixed:"1,3"`
-	DATECOMPTABLE       string `fix:"4,11"`
-	TYPEPIECE           string `fix:"12,13"`
-	GENERAL             string `fix:"14,30"`
-	TYPECPTE            string `fix:"31,31"`
-	AUXILIAIREOUSECTION string `fix:"32,48"`
-	REFINTERNE          string `fix:"49,83"`
-	LIBELLE             string `fix:"84,118"`
-	MODEPAIE            string `fix:"119,121"`
-	ECHEANCE            string `fix:"122,129"`
-	SENS                string `fix:"130,130"`
-	MONTANT1            string `fix:"131,150"`
-	TYPE_ECRITURE       string `fix:"151,151"`
-	NUMEROPIECE         string `fix:"152,159"`
-	DEVISE              string `fix:"160,162"`
-	TAUXDEV             string `fix:"163,172"`
-	CODEMONTANT         string `fix:"173,175"`
-	MONTANT2            string `fix:"176,195"`
-	MONTANT3            string `fix:"196,215"`
-	ETABLISSEMENT       string `fix:"216,218"`
-	AXE                 string `fix:"219,220"`
-	NUMECHE             string `fix:"221,222"`
+	DATECOMPTABLE       string `fixed:"4,11"`
+	TYPEPIECE           string `fixed:"12,13"`
+	GENERAL             string `fixed:"14,30"`
+	TYPECPTE            string `fixed:"31,31"`
+	AUXILIAIREOUSECTION string `fixed:"32,48"`
+	REFINTERNE          string `fixed:"49,83"`
+	LIBELLE             string `fixed:"84,118"`
+	MODEPAIE            string `fixed:"119,121"`
+	ECHEANCE            string `fixed:"122,129"`
+	SENS                string `fixed:"130,130"`
+	MONTANT1            string `fixed:"131,150"`
+	TYPE_ECRITURE       string `fixed:"151,151"`
+	NUMEROPIECE         string `fixed:"152,159"`
+	DEVISE              string `fixed:"160,162"`
+	TAUXDEV             string `fixed:"163,172"`
+	CODEMONTANT         string `fixed:"173,175"`
+	MONTANT2            string `fixed:"176,195"`
+	MONTANT3            string `fixed:"196,215"`
+	ETABLISSEMENT       string `fixed:"216,218"`
+	AXE                 string `fixed:"219,220"`
+	NUMECHE             string `fixed:"221,222"`
 }
